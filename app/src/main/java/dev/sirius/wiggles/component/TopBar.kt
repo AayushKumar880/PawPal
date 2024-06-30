103b5e70c441c87290758259fde056a8d3fb63e2
Aayush Kumar <157108425+AayushKumar880@users.noreply.github.com>
2024-06-30 12:32:29 +0530

    Update TopBar.kt

--- app/src/main/java/dev/sirius/wiggles/component/TopBar.kt
+++ app/src/main/java/dev/sirius/wiggles/component/TopBar.kt
@@ -34,7 +34,7 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import dev.spikeysanju.wiggles.R
+import dev.sirius.wiggles.R
 
 @Composable
 fun TopBar(onToggle: () -> Unit) {
